65527bd147b542e342217fcf4568a3b9bfe2de50
Baeji77 <hahawjstk@gmail.com>
2020-03-03 18:15:08 +0900

    Check validation about ArticleRequestDto in createArticle Check title, backgroundUrl, contents.

--- src/test/java/com/boot/spring/with/blog_man_big_to_growth/contorller/ArticleControllerTest.java
+++ src/test/java/com/boot/spring/with/blog_man_big_to_growth/contorller/ArticleControllerTest.java
@@ -1,12 +1,60 @@
 package com.boot.spring.with.blog_man_big_to_growth.contorller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ArticleControllerTest {
-    @Test
-    public void createArticle() {
+    @Autowired
+    private MockMvc mockMvc;
 
+    private final String URL_PATH = "/articles";
+    private final String JSON_CONTENT_TYPE = "application/json";
+
+    @Test
+    public void isFailCreateArticleWithEmptyJson() throws Exception {
+        mockMvc.perform(post(URL_PATH).contentType(JSON_CONTENT_TYPE).content("{}"))
+                .andExpect(status().is4xxClientError())
+                .andDo(print());
+    }
+
+    @Test
+    public void isFailCreateArticleWithoutTitle() throws Exception {
+        String jsonWithoutTitle = "{\"contents\": \"hi\", \"backgrroundUrl\": \"hi\"}";
+        mockMvc.perform(post(URL_PATH).contentType(JSON_CONTENT_TYPE).content(jsonWithoutTitle))
+                .andExpect(status().is4xxClientError())
+                .andDo(print());
+    }
+
+    @Test
+    public void isFailCreateArticleWithoutContents() throws Exception {
+        String jsonWithoutContents = "{\"title\": \"hi\", \"backgrroundUrl\": \"hi\"}";
+        mockMvc.perform(post(URL_PATH).contentType(JSON_CONTENT_TYPE).content(jsonWithoutContents))
+                .andExpect(status().is4xxClientError())
+                .andDo(print());
+    }
+
+    @Test
+    public void isSuccessCreateArticleWithoutBackgroundUrl() throws Exception {
+        String jsonWithoutBackgroundUrl = "{\"title\": \"hi\", \"contents\": \"hi\"}";
+        mockMvc.perform(post(URL_PATH).contentType(JSON_CONTENT_TYPE).content(jsonWithoutBackgroundUrl))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    public void isSuccessCreateArticleWithCorrectJson() throws Exception {
+        String jsonWithAllData = "{\"title\": \"hi\", \"contents\": \"hi\", \"backgrroundUrl\": \"hi\"}";
+        mockMvc.perform(post(URL_PATH).contentType(JSON_CONTENT_TYPE).content(jsonWithAllData))
+                .andExpect(status().isOk())
+                .andDo(print());
     }
 }
