b59d85d3f828177e9fd61a35dbdaab15efde625d
Baeji77 <hahawjstk@gmail.com>
2020-03-05 16:41:38 +0900

    Refactoring ArticleService to clean.

--- src/main/java/com/boot/spring/with/blog_man_big_to_growth/service/ArticleService.java
+++ src/main/java/com/boot/spring/with/blog_man_big_to_growth/service/ArticleService.java
@@ -2,11 +2,11 @@ package com.boot.spring.with.blog_man_big_to_growth.service;
 
 import com.boot.spring.with.blog_man_big_to_growth.dto.ArticleRequestDto;
 import com.boot.spring.with.blog_man_big_to_growth.dto.ArticleResponseDto;
-import com.boot.spring.with.blog_man_big_to_growth.dto.ArticleUpdateDto;
 import com.boot.spring.with.blog_man_big_to_growth.model.Article;
 import com.boot.spring.with.blog_man_big_to_growth.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -17,35 +17,39 @@ public class ArticleService {
         this.articleRepository = articleRepository;
     }
 
-    public void createNewArticle (ArticleRequestDto articleRequestDto) {
-        String title = articleRequestDto.getTitle();
-        String backgroundUrl = articleRequestDto.getBackgroundUrl();
-        String contents = articleRequestDto.getContents();
+    public ArticleResponseDto createNewArticle (ArticleRequestDto articleRequestDto) {
+        Article savedArticle = articleRepository.save(articleRequestDto.makeArticle());
+        log.info("{}", savedArticle);
 
-        Article article = new Article(title, backgroundUrl, contents);
-        Article savedArticle = articleRepository.save(article);
+        return articleToArticleResponseDto(savedArticle);
     }
 
     public ArticleResponseDto findOneArticleById (Long id) {
-        Article article = articleRepository.findById(id).orElseThrow(NullPointerException::new);
-        ArticleResponseDto articleResponseDto =
-                new ArticleResponseDto(article.getId(), article.getTitle(), article.getBackgroundUrl(), article.getContents());
+        Article foundArticle = articleRepository.findById(id).orElseThrow(NullPointerException::new);
+        ArticleResponseDto articleResponseDto = articleToArticleResponseDto(foundArticle);
+
         log.info("{}", articleResponseDto);
         return articleResponseDto;
     }
 
-    public void updateArticle (ArticleUpdateDto articleUpdateDto) {
-        Article article = Article.builder()
-                .id(articleUpdateDto.getId())
-                .title(articleUpdateDto.getTitle())
-                .contents(articleUpdateDto.getContents())
-                .backgroundUrl(articleUpdateDto.getBackgroundUrl())
-                .build();
-        Article savedArticle = articleRepository.save(article);
-        log.info("{}", savedArticle);
+    @Transactional
+    public ArticleResponseDto updateArticle (Long id, ArticleRequestDto articleRequestDto) {
+        Article foundArticle = articleRepository.findById(id).orElseThrow(NullPointerException::new);
+        foundArticle.update(articleRequestDto.getTitle(), articleRequestDto.getBackgroundUrl(), articleRequestDto.getContents());
+        log.info("{}", foundArticle);
+        return articleToArticleResponseDto(foundArticle);
     }
 
     public void deleteArticle (Long articleId) {
         articleRepository.deleteById(articleId);
+    }
+
+    private ArticleResponseDto articleToArticleResponseDto (Article article) {
+        return ArticleResponseDto.builder()
+                .id(article.getId())
+                .contests(article.getContents())
+                .backgroundUrl(article.getBackgroundUrl())
+                .title(article.getTitle())
+                .build();
     }
 }
